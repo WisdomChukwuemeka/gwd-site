0b68c3d1cf8f7dd724bf3826d06f3db82cae9e9c
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2025-04-11 08:01:59 +0100

    first commit

---
+++ pages/home.jsx
@@ -0,0 +1,18 @@
+export const Home = () => {
+    return(
+        <>
+            <div className="home-section-one">
+
+            
+            <div className="home-section">
+                <div className="home-container">
+                    <div className="title-container">
+                    <h1 className="learn">Learn <span>Web Development</span></h1>
+                    <h1 className="shape">and Shape your Future</h1>
+                    </div> 
+                </div>
+            </div>
+            </div>
+        </>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Layout } from "../pages/layout"
+import { Home } from "../pages/home"
+export const App = () => {
+  return(
+    <>
+      <div>
+        <Layout />
+        <Home />
+      </div>
+    </>
+  )
+}
